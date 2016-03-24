7584ccba2e323545624bb88c05c002fea7cb7a06
Anand Ramakrishna <anandram@microsoft.com>
2016-03-24 21:46:13 +0000

    fixes

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
 CONTAINERREPO="azlinux-libguestfs"
 CONTAINERTAG="0.01"
 CONTAINERNAME="$CONTAINERREPO" + "$CONTAINERTAG"
 
-CONTAINERID=$(sudo docker build -t "$CONTAINERNAME" . | grep "Successfully built" | awk '{print $3}')
+CONTAINERID=$(docker build -t "$CONTAINERNAME" . | grep "Successfully built" | awk '{print $3}')
 
 # if the export directory option is set, then export this built container to the directory
 if [ "$EXPORTDIR" ];then
-    sudo docker save $CONTAINERID > $EXPORTDIR/$CONTAINERNAME.tar
+    docker save $CONTAINERID > $EXPORTDIR/$CONTAINERNAME.tar
 fi
 
 
