cc3c30a1bed28f8138f8a38fec691c1de1923e2e
Amit Chattopadhyay <amitchat@gmail.com>
2016-09-21 22:02:10 +0000

    Add mode support and results.txt.

--- src/index.py
+++ src/index.py
@@ -56,8 +56,8 @@ class GuestFishWrapper:
 
         if (error.find('libguestfs: error') == -1 or
                 output.find('libguestfs:error') == -1):
-            return True
-        return False
+            return True, output
+        return False, output
 
     def execute(self, storageUrl, outputDirName):
         logging.info(storageUrl)
@@ -66,119 +66,143 @@ class GuestFishWrapper:
         requestDir = outputDirName + os.sep + timeStr
         varlogDir = requestDir + os.sep + 'var' + os.sep + 'log'
         etcDir = requestDir + os.sep + 'etc'
+        operationOutFilename = requestDir + os.sep + 'results.txt'
         os.makedirs(varlogDir)
         os.makedirs(etcDir)
-        
-        # Run guestfish in remote mode and then send it a command
-        # at a time since the programming environment inside guestfish
-        # is very limited (no variables, etc.)
 
-        args = [
-            '/libguestfs/run', 'guestfish', '--listen', 
-            '-a', storageUrl, '--ro' ]
-        logging.info(args)
+        with open(operationOutFilename, "w", newline="\r\n") as operationOutFile:       
 
-        # Guestfish server mode returns a string of the form
-        #   GUESTFISH_PID=pid; export GUESTFISH_PID
-        # We need to parse this and extract out the GUESTFISH_PID 
-        # environment variable and inserting it into the subsequent env
-        logging.info('Calling guestfish')
-        self.environment = os.environ.copy()
-        output = subprocess.check_output(
-            args, env=self.environment, universal_newlines=True)
-        logging.info(output)
+            # Run guestfish in remote mode and then send it a command
+            # at a time since the programming environment inside guestfish
+            # is very limited (no variables, etc.)
 
-        try:
-            guestfishpid = int(output.split(';')[0].split('=')[1])
-        except Exception as e:
-            raise Exception('Cannot find GUESTFISH_PID')
-        
-        self.environment['GUESTFISH_PID'] = str(guestfishpid)
-        logging.info('GUESTFISH_PID = %d', guestfishpid)
+            args = [
+                '/libguestfs/run', 'guestfish', '--listen', 
+                '-a', storageUrl, '--ro' ]
+            logging.info(args)
 
-        # Enumerate file systems
-        # Then try mounting them and looking for logs
-        # Exit out once any logs are found
-
-        self.callGF('Launching', ['launch'])
-        output, error = self.callGF('Listing filesystems', ['list-filesystems'])
-
-        # output of list-filesystems is of the form:
-        #   /dev/sda1: ext4
-        #   /dev/sdb1: ext4 
-        #   ...
-
-        for line in output.splitlines():
-            idx = line.find(':')
-            if idx == -1:
-                continue
-            device = line[:idx]
-            logging.info('Found device at path: %s', device)
+            # Guestfish server mode returns a string of the form
+            #   GUESTFISH_PID=pid; export GUESTFISH_PID
+            # We need to parse this and extract out the GUESTFISH_PID 
+            # environment variable and inserting it into the subsequent env
+            logging.info('Calling guestfish')
+            self.environment = os.environ.copy()
+            output = subprocess.check_output(
+                args, env=self.environment, universal_newlines=True)
+            logging.info(output)
 
             try:
-                self.callGF('Unmounting root volume', 
-                    ['--', '-umount', '/'], True)
-            except subprocess.CalledProcessError:
-                pass
-
-            failed = False
-            try:
-                if self.validateGF('Trying to mounting %s' %(device), 
-                        ['--', '-mount', device, '/']) != True:
-                    failed = True
-            except subprocess.CalledProcessError as e:
-                failed = True
-
-            if failed == True:
-                # Couldn't mount this device, so just continue to next device
-                logging.info('Could not mount device %s', device)
-                continue
-
-            # Look for existence of /var/log
-            failed = False
-            try:
-                if self.validateGF(
-                        'Looking for existence of /var/log', 
-                        ['--', '-ll', '/var/log']) != True:
-                    failed = True
-            except subprocess.CalledProcessError as e:
-                failed = True
-
-            if failed == True:
-                logging.info('/var/log does not exist on %s', device)
-                continue
-
-            self.callGF('Copying waagent logs', 
-                ['--','-glob','copy-out', '/var/log/waagent*', varlogDir], True)
-            self.callGF('Copying syslog files',
-                ['--','-glob', 'copy-out', '/var/log/syslog*', varlogDir], True)
-            self.callGF('Copying rsyslog files',
-                ['--','-glob', 'copy-out','/var/log/rsyslog*', varlogDir], True)
-            self.callGF('Copying messages',
-                ['--','-glob','copy-out','/var/log/messages*', varlogDir], True)
-            self.callGF('Copying kern logs',
-                ['--', '-glob', 'copy-out', '/var/log/kern*', varlogDir], True)
-            self.callGF('Copying dmesg logs',
-                ['--', '-glob', 'copy-out', '/var/log/dmesg*', varlogDir], True)
-            self.callGF('Copying dpkg logs',
-                ['--', '-glob', 'copy-out', '/var/log/dpkg*', varlogDir], True)
-            self.callGF('Copying yum logs',
-                ['--', '-glob', 'copy-out', '/var/log/yum*', varlogDir], True)
-            self.callGF('Copying cloud-init logs',
-                ['--','-glob','copy-out','/var/log/cloud-init*',varlogDir], True)
-            self.callGF('Copying boot logs',
-                ['--','-glob', 'copy-out', '/var/log/boot*', varlogDir], True)
-            self.callGF('Copying auth logs',
-                ['--', '-glob', 'copy-out', '/var/log/auth*', varlogDir], True)
-            self.callGF('Copying secure logs',
-                ['--','-glob', 'copy-out', '/var/log/secure*', varlogDir], True)
-            self.callGF('Copying fstab',
-                ['--','-glob', 'copy-out', '/etc/fstab', etcDir], True)
-            self.callGF('Copying sshd_conf',
-                ['--','-glob', 'copy-out', '/etc/ssh/sshd_config', etcDir],True)
+                guestfishpid = int(output.split(';')[0].split('=')[1])
+            except Exception as e:
+                raise Exception('Cannot find GUESTFISH_PID')
             
-        self.callGF('Exiting guestfish', ['--', '-exit'])
-        logging.info('Guestfish done!')
+            self.environment['GUESTFISH_PID'] = str(guestfishpid)
+            logging.info('GUESTFISH_PID = %d', guestfishpid)
+
+            # Enumerate file systems
+            # Then try mounting them and looking for logs
+            # Exit out once any logs are found
+
+            self.callGF('Launching', ['launch'])
+            operationOutFile.write("Filesystem Status:\r\n")
+            output, error = self.callGF('Listing filesystems', ['list-filesystems'])
+            operationOutFile.write(output)
+            operationOutFile.write("\r\n")
+
+            # output of list-filesystems is of the form:
+            #   /dev/sda1: ext4
+            #   /dev/sdb1: ext4 
+            #   ...
+
+            for line in output.splitlines():
+                idx = line.find(':')
+                if idx == -1:
+                    continue
+                device = line[:idx]
+                logging.info('Found device at path: %s', device)
+
+                try:
+                    self.callGF('Unmounting root volume', 
+                        ['--', '-umount', '/'], True)
+                except subprocess.CalledProcessError:
+                    pass
+
+                failed = False
+                try:
+                    completed, output = self.validateGF('Trying to mounting %s' %(device), 
+                            ['--', '-mount', device, '/']) 
+                    if completed != True:
+                        failed = True
+                    else:
+                        operationOutFile.write("Mounted ")
+                        operationOutFile.write(device)
+                        operationOutFile.write("\r\n")
+                except subprocess.CalledProcessError as e:
+                    failed = True
+
+                if failed == True:
+                    operationOutFile.write("Failed to mount ")
+                    operationOutFile.write(device)
+                    operationOutFile.write("\r\n")
+                    # Couldn't mount this device, so just continue to next device
+                    logging.info('Could not mount device %s', device)
+                    continue
+
+                operationOutFile.write("Listing /var/log:\r\n")
+                # Look for existence of /var/log
+                failed = False
+                try:
+                    completed, output = self.validateGF(
+                            'Looking for existence of /var/log', 
+                            ['--', '-ll', '/var/log'])
+                    if completed != True:
+                        failed = True
+                    else:
+                        operationOutFile.write(output)
+                        operationOutFile.write("\r\n")
+                except subprocess.CalledProcessError as e:
+                    failed = True
+
+                if failed == True:
+                    logging.info('/var/log does not exist on %s', device)
+                    operationOutFile.write("n/a\r\n")
+                    continue
+
+                self.callGF('Copying waagent logs', 
+                    ['--','-glob','copy-out', '/var/log/waagent*', varlogDir], True)
+                self.callGF('Copying syslog files',
+                    ['--','-glob', 'copy-out', '/var/log/syslog*', varlogDir], True)
+                self.callGF('Copying rsyslog files',
+                    ['--','-glob', 'copy-out','/var/log/rsyslog*', varlogDir], True)
+                self.callGF('Copying messages',
+                    ['--','-glob','copy-out','/var/log/messages*', varlogDir], True)
+                self.callGF('Copying kern logs',
+                    ['--', '-glob', 'copy-out', '/var/log/kern*', varlogDir], True)
+                self.callGF('Copying dmesg logs',
+                    ['--', '-glob', 'copy-out', '/var/log/dmesg*', varlogDir], True)
+                self.callGF('Copying dpkg logs',
+                    ['--', '-glob', 'copy-out', '/var/log/dpkg*', varlogDir], True)
+                self.callGF('Copying yum logs',
+                    ['--', '-glob', 'copy-out', '/var/log/yum*', varlogDir], True)
+                self.callGF('Copying cloud-init logs',
+                    ['--','-glob','copy-out','/var/log/cloud-init*',varlogDir], True)
+                self.callGF('Copying boot logs',
+                    ['--','-glob', 'copy-out', '/var/log/boot*', varlogDir], True)
+                self.callGF('Copying auth logs',
+                    ['--', '-glob', 'copy-out', '/var/log/auth*', varlogDir], True)
+                self.callGF('Copying secure logs',
+                    ['--','-glob', 'copy-out', '/var/log/secure*', varlogDir], True)
+                self.callGF('Copying fstab',
+                    ['--','-glob', 'copy-out', '/etc/fstab', etcDir], True)
+                self.callGF('Copying sshd_conf',
+                    ['--','-glob', 'copy-out', '/etc/ssh/sshd_config', etcDir],True)
+                
+            self.callGF('Exiting guestfish', ['--', '-exit'])
+
+            proc = subprocess.Popen(["ls","-R",requestDir], stdout=subprocess.PIPE)
+            output = proc.stdout.read().decode('UTF-8')
+            operationOutFile.write("Packaged files:\r\n")
+            operationOutFile.write(output)
 
         logging.info('Making archive')
         archiveName = shutil.make_archive(requestDir, 'zip', requestDir)
@@ -196,12 +220,13 @@ class GuestFishHttpHandler(http.server.BaseHTTPRequestHandler):
         try:
             urlObj = urllib.parse.urlparse(self.path)
             urlSplit = urlObj.path.split('/')
-            if not len(urlSplit) >= 4:
+            if not len(urlSplit) >= 5:
                 return
 
             operationId = urlSplit[1]
-            storageAcctName = urlSplit[2]
-            container_blob_name = urlSplit[3] + '/' + urlSplit[4]
+            mode = urlSplit[2]
+            storageAcctName = urlSplit[3]
+            container_blob_name = urlSplit[4] + '/' + urlSplit[5]
             storageUrl = urllib.parse.urlunparse(('https', storageAcctName + '.blob.core.windows.net', container_blob_name, '', urlObj.query, None))        
         
             logging.info('Processing operation id# ' + operationId)
